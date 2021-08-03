e87dfc4f75a19620be13a8db25ca7000df55be7f
Samuel Prigent <samuel.prigent@yahoo.fr>
2021-08-03 17:40:35 +0200

    début inner html avec js

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+// on importe l'object teddies
+// import { teddies } from '../backend/models/Teddy';
+
+// on modifie l'html 
+let product1 = document.querySelector('#product1-name');
+product1.innerHTML = "Robert JS";
+
+
